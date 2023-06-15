03c630cd91887db4e4559a80dcd5401421000e9b
sakshi8707 <96986663+sakshi8707@users.noreply.github.com>
2023-06-15 16:18:45 +0530

    after tooltip

--- Personal Projects/PORTFOLIO PROJECT/index1.js
+++ Personal Projects/PORTFOLIO PROJECT/index1.js
@@ -10,9 +10,50 @@ const textLoad = () => {
   setTimeout(() => {
     text.textContent = "नमस्ते |";
   }, 8000);
-
 };
-
 
 textLoad();
 setInterval(textLoad, 12000);
+
+// changing button text on hover in JavaScript e
+
+// var hoverButtons = document.querySelectorAll(".hover-button");
+// var hoverTexts = ["Visitor Chat", "Home", "Portfolio", "Coming Soon", "Theme"];
+
+// hoverButtons.forEach(function (button, index) {
+//   var hoverText = hoverTexts[index];
+//   var hoverTextElement = document.createElement("div");
+//   hoverTextElement.className = "hover-text";
+//   hoverTextElement.innerText = hoverText;
+//   button.appendChild(hoverTextElement);
+
+//   button.addEventListener("mouseover", function () {
+//     hoverTextElement.style.display = "block";
+//   });
+
+//   button.addEventListener("mouseout", function () {
+//     hoverTextElement.style.display = "none";
+//   });
+// });
+
+// *Navbar*
+
+const plusIcon = document.querySelector(".plus-icon");
+const navTop = document.querySelector(".navbar-top");
+const navRight = document.querySelector(".navbar-right");
+var activeBtn=0;
+
+const navAnimation = function () {
+  plusIcon.classList.add("rotate");
+  setTimeout(function () {
+    plusIcon.classList.remove("rotate");
+    plusIcon.classList.add("fa-plus");
+  }, 2000);
+  
+  navTop.style.transform = "translateX(0vh)";
+  navRight.style.transform = "translateY(0px)";
+  const isAdult = activeBtn==0  ? 1 : 0;  
+};
+plusIcon.addEventListener("click", navAnimation);
+
+
