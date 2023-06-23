434b4283b15fdcc5d20e8de18d858bc0554ad129
sakshi8707 <96986663+sakshi8707@users.noreply.github.com>
2023-06-24 00:12:13 +0530

    till Portfolio Grid

--- Personal Projects/PORTFOLIO PROJECT/index1.js
+++ Personal Projects/PORTFOLIO PROJECT/index1.js
@@ -18,7 +18,6 @@ setInterval(textLoad, 12000);
 // changing button text on hover in JavaScript e
 
 // *Navbar*
-
 const plusIcon = document.querySelector(".plus-icon");
 const navTop = document.querySelector(".navbar-top");
 const navRight = document.querySelector(".navbar-right");
@@ -29,6 +28,7 @@ let addStyle = function () {
   navTop.style.transform = "translateX(0px)";
   navRight.style.transform = "translateY(0px)";
   plusIcon.classList.add("rotate");
+  navRight.classList.add("show"); // Add the "show" class to display the right navbar
   setTimeout(function () {
     plusIcon.classList.remove("rotate");
     plusIcon.classList.add("fa-white");
@@ -38,8 +38,9 @@ let addStyle = function () {
 let closeStyle = function () {
   plusIcon.classList.add("revrotate");
   navTop.style.transform = "translateX(-100vw)";
-  navRight.style.transform = "translateY(-100vh)";
+  navRight.style.transform = "translateY(-100%)"; // Use percentage value for smooth transition
   plusIcon.classList.remove("fa-white");
+  navRight.classList.remove("show"); // Remove the "show" class to hide the right navbar
   setTimeout(function () {
     plusIcon.classList.remove("revrotate");
   }, 2000);
@@ -67,5 +68,40 @@ plusIcon.addEventListener("click", function () {
   // Toggle the variable to switch the class
   isActive = !isActive;
 });
-
+      
 // **Portfolio section**
+// done
+
+
+// **Star Paritcles section**
+
+// Generate a specified number of stars
+function generateStars(numStars) {
+  var container = document.getElementById('star-container');
+
+  for (var i = 0; i < numStars; i++) {
+    var star = document.createElement('div');
+    star.className = 'star';
+    container.appendChild(star);
+  }
+}
+
+// Animate the stars
+function animateStars() {
+  var stars = document.getElementsByClassName('star');
+
+  for (var i = 0; i < stars.length; i++) {
+    var star = stars[i];
+    var delay = Math.random() * 5;
+    var duration = Math.random() * 5 + 5;
+
+    star.style.animationDelay = delay + 's';
+    star.style.animationDuration = duration + 's';
+  }
+}
+
+// Generate and animate stars when the page loads
+window.onload = function() {
+  generateStars(100); // Adjust the number of stars as desired
+  animateStars();
+};
